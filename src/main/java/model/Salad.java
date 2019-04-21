afd76ee06e6d7335c1d3873cded172887c518789
Shchava <yarosshc@gmail.com>
2019-04-21 16:40:16 +0300

    add constructors of Salad

--- src/main/java/model/Salad.java
+++ src/main/java/model/Salad.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Salad {
+    private List<FoodIngredient> ingredients;
+
+    public Salad(){
+        ingredients = new ArrayList<FoodIngredient>();
+    }
+
+    public Salad(FoodIngredient... ingredients){
+        this.ingredients = Arrays.asList(ingredients);
+    }
+
+    public Salad(List<FoodIngredient> ingredients){
+        this.ingredients = ingredients;
+    }
+
+    public boolean addIngredient(FoodIngredient ingredient){
+        return ingredients.add(ingredient);
+    }
+
+
+
 }
