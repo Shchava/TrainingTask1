34e16d9500e1ce40248b462340c517ca1044d1c4
Shchava <yarosshc@gmail.com>
2019-04-21 21:16:15 +0300

    add generate random salad method and help methods

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,4 +1,35 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Model {
+    public final double MAX_INGREDIENT_WEIGHT = 2;
+    public final double MIN_INGREDIENT_WEIGHT = 0.01;
+
+    public Salad makeRandomSalad(){
+        Salad salad = new Salad();
+        HashSet<Integer> used = new HashSet<Integer>();
+        for(int i = 0; i < 5; i++){
+            VegetableType newIngredientType = generateRandomVegetable(used);
+            double newIngredientWeight = generateRandomWeight();
+
+            Vegetable newIngredient = new Vegetable(newIngredientType,newIngredientWeight);
+            salad.addIngredient(newIngredient);
+        }
+        return salad;
+    }
+
+    private VegetableType generateRandomVegetable(Set<Integer> used){
+        int generated;
+        do{
+            generated = (int)(VegetableType.values().length * Math.random());
+        }while(used.contains(generated));
+        used.add(generated);
+        return VegetableType.values()[generated];
+    }
+
+    private double generateRandomWeight(){
+        return (MAX_INGREDIENT_WEIGHT - MIN_INGREDIENT_WEIGHT) * Math.random() + MIN_INGREDIENT_WEIGHT;
+    }
 }
