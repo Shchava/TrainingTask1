ad904e98af65ffe22d8c56daaeba2339e1a9eb98
Shchava <yarosshc@gmail.com>
2019-04-21 18:30:03 +0300

    methods of Salad class declaration

--- src/main/java/model/Salad.java
+++ src/main/java/model/Salad.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Salad {
+public class Salad implements EatAble{
     private List<FoodIngredient> ingredients;
 
     public Salad(){
@@ -23,6 +23,19 @@ public class Salad {
         return ingredients.add(ingredient);
     }
 
+    public List<FoodIngredient>getIngredients(){
+        return ingredients;
+    }
 
+    public void sortByCalories(){};
 
+    public void sortByWeight(){};
+
+    public void getIngredientsMatchesCaloriesRange(int minCalories,int maxCalories){
+
+    }
+
+    public int getCalories() {
+        return 0;
+    }
 }
