65a25ef5e6d867528132d902213e145b0c029d8c
Shchava <yarosshc@gmail.com>
2019-04-22 00:08:45 +0300

    add resource bundle for messages

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,6 +1,12 @@
 package view;
 
+import java.util.ResourceBundle;
+
 public class View {
+    public static final String MESSAGES_BUNDLE_NAME = "Messages";
+
+    public ResourceBundle Messages = ResourceBundle.getBundle(MESSAGES_BUNDLE_NAME);
+
     public void print(String message){
         System.out.println(message);
     }
