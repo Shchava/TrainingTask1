ba9600efa849753de9f732c87aab52f14f9f71df
Shchava <yarosshc@gmail.com>
2019-04-22 11:56:37 +0300

    TestModel class refactoring

--- src/test/java/TestModel.java
+++ src/test/java/TestModel.java
@@ -5,19 +5,21 @@ import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestModel {
-    Model testModel = new Model();
+class TestModel {
+    private Model testModel = new Model();
 
     @Test
-    public void testMakeRandomSalad(){
+    void testMakeRandomSalad(){
         for(int i = 0; i < 1000; i++){
             HashSet<VegetableType> unique = new HashSet<>();
 
             Salad testSalad = testModel.makeRandomVegetableSalad();
 
             for(FoodIngredient ingredient : testSalad.getIngredients()){
+                //check uniqueness of created ingredients
                 assertTrue(unique.add(((Vegetable)ingredient).getType()));
 
+                //check if weights of created salad is in range
                 assertTrue(ingredient.getWeight() > testModel.MIN_INGREDIENT_WEIGHT);
                 assertTrue(ingredient.getWeight() < testModel.MAX_INGREDIENT_WEIGHT);
             }
